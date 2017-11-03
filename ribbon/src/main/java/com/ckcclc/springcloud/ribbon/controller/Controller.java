fada6495f130f70b883b9098bdfb0e7da5ac40fc
huangyucong <huangyucong@sensetime.com>
2017-11-03 19:25:30 +0800

    update

--- ribbon/src/main/java/com/ckcclc/springcloud/ribbon/controller/Controller.java
+++ ribbon/src/main/java/com/ckcclc/springcloud/ribbon/controller/Controller.java
@@ -1,7 +1,7 @@
 package com.ckcclc.springcloud.ribbon.controller;
 
-import com.ckcclc.springcloud.configserver.service.RemoteService;
 
+import com.ckcclc.springcloud.ribbon.service.RemoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
